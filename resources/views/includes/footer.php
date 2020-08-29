9c3df0bd015825d69a8885f6d1ebd4b8c01c8681
Ahmed magdy <ahmedmagdu1@gmail.com>
2020-08-29 13:15:30 +0200

    UI install & ui bootstrap & auth

---
+++ resources/views/includes/footer.php
@@ -0,0 +1,47 @@
+      <!-- FOOTER -->
+      <footer class="container-fluid footer-section">
+	  <div class="row">
+		<div class="col-md-6">
+		<div class="row">
+		<div class="col-md-3">
+		<img class="img-fluid footer-logo" src="images/logo/savvyous-logo.png" alt="Savvyous">
+		</div>
+		<div class="col-md-9">
+		<h3>Connecting<br>Bright Minds</h3>
+		</div>
+		</div>
+		
+		</div>
+		<div class="col-md-3 ">
+		<ul class="list-unstyled">
+		<li><a href="#">Privacy & Terms</a></li>
+		<li><a href="#">Terms & Condetions</a></li>
+		<li><a href="#">Cookies Policy</a></li>
+		</ul>
+		</div>
+		
+		<div class="col-md-3">
+		<ul class="list-unstyled">
+		<li><a href="#">Blogs</a></li>
+		<li><a href="#">Contact us</a></li>
+		<li><a href="#">About us</a></li>
+		</ul>
+		</div>
+		
+		
+	  
+	  </div>
+      </footer>
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.7/holder.min.js"></script>
+  </body>
+</html>
---
+++ resources/views/includes/header.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>savvyous</title>
+
+    <link rel="canonical" href="https://savvyous.com">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+	
+	   <!-- Font Awesome 4.7.0 -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg">
+  <a class="navbar-brand" href="#"><img src="images/logo/white-logo.png"/></a>
+    <form class="form-inline">
+  <div class="form-group col-auto has-search">
+    <span class="fa fa-search form-control-feedback"></span>
+    <input type="text" class="form-control" placeholder="Search for Projects, teams, Companies etc.">
+  </div>
+    </form>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">
+          <i class="fa fa-home"></i>
+          Home
+          <span class="sr-only">(current)</span>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-suitcase">
+            <span class="badge badge-danger">11</span>
+          </i>
+          Post Project
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-envelope">
+            <span class="badge badge-warning">11</span>
+          </i>
+          Mesages
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link " href="#" >
+          <i class="fa fa-bell">
+            <span class="badge badge-primary">11</span>
+          </i>
+          Notifications
+        </a>
+
+      </li>
+    </ul>
+	
+	
+    <ul class="navbar-nav  ">
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-user">
+            <span class="badge badge-info">11</span>
+          </i>
+          Profile
+        </a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+
+    </header>
